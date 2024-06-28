3a034a86f76acb3f0ddba79c0f7dc83b656c609e
wch <wonchiho9@gamil.com>
2024-06-28 16:42:50 +0900

    customer컨트롤러, 서비스 수정

--- WebBucks_backend/src/main/java/com/webbucks/customer/Controller/CustomerController.java
+++ WebBucks_backend/src/main/java/com/webbucks/customer/Controller/CustomerController.java
@@ -1,20 +1,21 @@
 package com.webbucks.customer.Controller;
 
-import com.webbucks.admin.Service.AdminService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.webbucks.customer.Service.CustomerService;
 
 @RestController
 @RequestMapping(value = "/api")
 public class CustomerController {
 	
 	@Autowired
-	private AdminService adminService;
+	private CustomerService customerService;
 
 	
-    @GetMapping("/test")
+    @GetMapping("/test11")
     public String myTest() {
         return "Hello, world!";
     }
