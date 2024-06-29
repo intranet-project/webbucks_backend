920009ce1caf26b4e491ab5f0563d5f3d5eb6cf1
YUBIN CHOE <cybin050300@naver.com>
2024-06-29 21:09:28 +0900

    인트라넷 응답 데이터

--- WebBucks_backend/src/main/java/com/webbucks/customer/Controller/CustomerController.java
+++ WebBucks_backend/src/main/java/com/webbucks/customer/Controller/CustomerController.java
@@ -1,6 +1,8 @@
 package com.webbucks.customer.Controller;
 
+import com.webbucks.Entity.Voice;
 import com.webbucks.customer.dto.voiceRequestDto;
+import com.webbucks.customer.dto.voiceResponseDto;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
@@ -18,6 +20,11 @@ public class CustomerController {
     public String saveVoice(@RequestBody voiceRequestDto voice) {
         customerService.saveVoice(voice);
         return "Hello, world!";
+    }
+
+    @GetMapping("/getvoice")
+    public Voice getVoice(@RequestParam("id") long voiceId) {
+        return customerService.getVoice(voiceId);
     }
 }
 
--- WebBucks_backend/src/main/java/com/webbucks/customer/Service/CustomerService.java
+++ WebBucks_backend/src/main/java/com/webbucks/customer/Service/CustomerService.java
@@ -1,8 +1,11 @@
 package com.webbucks.customer.Service;
 
 import com.webbucks.Entity.Customer;
+import com.webbucks.Entity.Voice;
 import com.webbucks.customer.dto.voiceRequestDto;
+import com.webbucks.customer.dto.voiceResponseDto;
 
 public interface CustomerService {
     void saveVoice(voiceRequestDto voice);
+    Voice getVoice(long voiceId);
 }
